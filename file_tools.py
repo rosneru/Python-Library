e4f2e82b331640ed7bc2d051303c1f2cce143f4e
Uwe Rosner <oscottmusic@posteo.de>
2019-02-06 16:51:23 +0100

    Adding a function to rename all files in a directory
    Furthermore all refernces to the old name *inside* the file will be
    replaced by the new name.

--- file_tools.py
+++ file_tools.py
@@ -34,3 +34,29 @@ def write_many_lines_to_file(file_name, number_of_lines):
   
   print("{0} lines written sucessfully to new created file '{1}'.".format(number_of_lines, file_name))
   output_file.close()
+
+def multi_insert_src_class_object(dir, extension, insert_text, 
+  insert_index):
+
+  """ This function renames all files with extension ``extension``
+  which are recursively found in directory ``dir``. It will insert the
+  ``insert_text`` at position ``insert_index`` into the file name.
+  It will stick to the camel case rule, so the first letter after the
+  inserted text will be a capital.
+  
+  In a second step it is reads the file itself search-and-replacing 
+  all occurences of the old file name with the new filename (without 
+  ``extension``).
+
+  In this way text can be inserted into a bunch of Java/C++/C# classes 
+  when the rule 'one-class-per-file' is adhered.
+  """
+  print("\nYou requested all '", extension ,"' files found in directory\n ", 
+    dir,"\nto get the text '", insert_text,"' inserted at position " , 
+    insert_index,"..\n\n", sep="")
+    
+  typed = input("Continue (y/n) ?")
+  print("You entered " + str(typed))
+
+if __name__ == "__main__":
+  multi_insert_src_class_object("d:\DEV\Kruess\kr_tools_KruessBench\KruessBench\Views-KR-RICO", ".cs",  "rico", 0)
