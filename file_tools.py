ba5410c3adac9df8609d54929a951745bbf601b8
Uwe Rosner <oscottmusic@posteo.de>
2019-02-08 15:02:59 +0100

    Adding a function to rename all files in a directory
    Furthermore all refernces to the old name *inside* the file will be
    replaced by the new name.

--- file_tools.py
+++ file_tools.py
@@ -1,6 +1,7 @@
 """ Collection of file tools to quickly create test files etc """
 
-import os.path
+from glob import glob
+from os.path import exists, join
 
 def write_many_lines_to_file(file_name, number_of_lines):
   """ This function generates a file with name ``file_name`` and fills 
@@ -16,7 +17,7 @@ def write_many_lines_to_file(file_name, number_of_lines):
   Line  99
   Line 100
   """
-  if os.path.exists(file_name):
+  if exists(file_name):
     print("Error: File already exists. Please provide another file name.")
     return
 
@@ -35,28 +36,44 @@ def write_many_lines_to_file(file_name, number_of_lines):
   print("{0} lines written sucessfully to new created file '{1}'.".format(number_of_lines, file_name))
   output_file.close()
 
-def multi_insert_src_class_object(dir, extension, insert_text, 
-  insert_index):
+def multi_insert_src_class_object(dir_path, file_extension, 
+  insert_text, insert_index):
 
-  """ This function renames all files with extension ``extension``
-  which are recursively found in directory ``dir``. It will insert the
-  ``insert_text`` at position ``insert_index`` into the file name.
-  It will stick to the camel case rule, so the first letter after the
-  inserted text will be a capital.
+  """ This function renames all files with extension ``file_extension``
+  which are recursively found in directory ``dir_path``. It will 
+  insert the ``insert_text`` at position ``insert_index`` into the file
+  name. It will stick to the camel case rule, so the first letter after
+  the inserted text will be a capital.
   
   In a second step it is reads the file itself search-and-replacing 
   all occurences of the old file name with the new filename (without 
-  ``extension``).
+  ``file_extension``).
 
   In this way text can be inserted into a bunch of Java/C++/C# classes 
   when the rule 'one-class-per-file' is adhered.
+
+  NOTE ``file_extension`` can be a tuple or list cover multiple 
+  extensions.
   """
-  print("\nYou requested all '", extension ,"' files found in directory\n ", 
-    dir,"\nto get the text '", insert_text,"' inserted at position " , 
+  print("\nYou requested all '", file_extension ,"' files found in directory\n ", 
+    dir_path,"\nto get the text '", insert_text,"' inserted at position " , 
     insert_index,"..\n\n", sep="")
     
-  typed = input("Continue (y/n) ?")
-  print("You entered " + str(typed))
+#  typed = input("Continue (y/n) ? ")
+#  if typed != "y":
+#    print("Cancel.")
+#    return
+
+  all_files = []
+
+  if isinstance(file_extension, (list, tuple)):
+    for extension in file_extension:
+      all_files.extend(glob(join(dir_path, extension)))
+  else:
+    all_files = glob(join(dir_path, file_extension))
+
+  print("Listing all files:\n", all_files)
 
 if __name__ == "__main__":
-  multi_insert_src_class_object("d:\DEV\Kruess\kr_tools_KruessBench\KruessBench\Views-KR-RICO", ".cs",  "rico", 0)
+  # multi_insert_src_class_object("d:\DEV\Kruess\kr_tools_KruessBench\KruessBench\Views-KR-RICO", ".cs",  "rico", 0)
+  multi_insert_src_class_object('e:\\Temp\\Testfiles', '*.cs',  'kric', 0)
