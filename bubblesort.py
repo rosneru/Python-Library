197eb414400d0dd50d8e827b00324bd1b8a539e8
Uwe Rosner <oscottmusic@posteo.de>
2019-05-14 11:50:27 +0200

    Finished basic bubble sort algorithm

--- bubblesort.py
+++ bubblesort.py
@@ -1,3 +1,11 @@
 def sort(input_list):
-  output_list = [2, 3, 1]
-  return output_list
+  a0 = 0                # first item id
+  d0 = len(input_list)  # number of items
+  for n in range(d0, 1, -1):
+    for i in range(0, n - 1):
+      if input_list[i] > input_list[i + 1]:
+        tmp = input_list[i]
+        input_list[i] = input_list[i + 1]
+        input_list[i + 1] = tmp
+
+  return input_list
--- bubblesort_tests.py
+++ bubblesort_tests.py
@@ -6,9 +6,13 @@ class BubblesortTests(unittest.TestCase):
     def test_bubblesort01(self):
         input_list = [12, 28, -1, -5, 24, 39, 17]
         output_list = bubblesort.sort(input_list)
-        self.assertEqual(output_list[0], 2)
-        self.assertEqual(output_list[1], 3)
-        self.assertEqual(output_list[2], 1)
+        self.assertEqual(output_list[0], -5)
+        self.assertEqual(output_list[1], -1)
+        self.assertEqual(output_list[2], 12)
+        self.assertEqual(output_list[3], 17)
+        self.assertEqual(output_list[4], 24)
+        self.assertEqual(output_list[5], 28)
+        self.assertEqual(output_list[6], 39)
 
 if __name__ == '__main__':
     unittest.main()
