9b2754847fd17ae3852e44c461b275ef7bf58bf3
Uwe Rosner <oscottmusic@posteo.de>
2019-05-14 12:20:11 +0200

    BubbleSort now doesn't overwrite input list

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,14 @@
+import copy
+
 def sort(input_list):
+  output_list = copy.deepcopy(input_list)
   a0 = 0                # first item id
-  d0 = len(input_list)  # number of items
+  d0 = len(output_list) # number of items
   for n in range(d0, 1, -1):
     for i in range(0, n - 1):
-      if input_list[i] > input_list[i + 1]:
-        tmp = input_list[i]
-        input_list[i] = input_list[i + 1]
-        input_list[i + 1] = tmp
+      if output_list[i] > output_list[i + 1]:
+        tmp = output_list[i]
+        output_list[i] = output_list[i + 1]
+        output_list[i + 1] = tmp
 
-  return input_list
+  return output_list
