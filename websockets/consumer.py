f742ecad8257974d19db442a6d850933e7be3a29
rosneru <oscottmusic@posteo.de>
2020-09-16 14:13:54 +0200

    Prepared consumer

--- websockets/consumer.py
+++ websockets/consumer.py
@@ -2,3 +2,17 @@ import asyncio
 import logging
 import websockets
 
+logging.basicConfig(level=logging.INFO)
+
+async def consumer_handler(websocket: WebSocketClientProtocol) -> None:
+    async for message in websocket:
+        log_message(message)
+
+async def consume(hostname: str, port: int) -> None:
+    websocket_resource_url = f"ws://{hostname}:{port}"
+    async with websockets.connect(websocket_resource_url) as websocket:
+        await consumer_handler(websocket)
+
+async def log_message(message: str) -> None:
+    logging.info(f"Message: {message}")
+
