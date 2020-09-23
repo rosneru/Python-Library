955443b61b6628f5942c1cd9ce863e3ab3718625
rosneru <oscottmusic@posteo.de>
2020-09-23 16:24:46 +0200

    Finished producer

--- websockets/producer.py
+++ websockets/producer.py
@@ -5,3 +5,7 @@ async def produce(message: str, host: str, port: int) -> None:
     async with websockets.connect(f"ws://{host}:{port}") as ws:
         await ws.send(message)
         await ws.recv()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(produce(message='hi', host='localhost', port=4000))
