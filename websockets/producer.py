287d59787818d1b2c5692d167697629e719226e4
rosneru <oscottmusic@posteo.de>
2020-09-23 12:36:15 +0200

    Implementing websocket example

---
+++ websockets/producer.py
@@ -0,0 +1,7 @@
+import asyncio
+import websockets
+
+async def produce(message: str, host: str, port: int) -> None:
+    async with websockets.connect(f"ws://{host}:{port}") as ws:
+        await ws.send(message)
+        await ws.recv()
